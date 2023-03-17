bb3953e707b99a076433647a7d2bac2a969238d5
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-17 04:41:13 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+let hqLocation = 42;
+let NumberOfBlocksFromHq = Math.abs(pickupLocation - hqLocation);
+return NumberOfBlocksFromHq
+}
+distanceFromHqInBlocks(34);
+
+
+function distanceFromHqInFeet(blockNumber){
+
+let distanceInFeet =  distanceFromHqInBlocks(blockNumber) * 264;
+return distanceInFeet
+}
+distanceFromHqInFeet(43);
+
+function distanceTravelledInFeet(startBlock, endBlock){
+let distance = Math.abs(startBlock - endBlock);
+return (distance * 264)
+}
+distanceTravelledInFeet(34,28);
+
+function calculatesFarePrice(start, destination){
+    let farePrice;
+let totalFeet = distanceTravelledInFeet(start, destination);
+if (totalFeet <= 400){farePrice = 0}
+
+    else if (totalFeet > 400 && totalFeet <=2000){farePrice = 0.02 * (totalFeet - 400)}
+
+    else if (totalFeet > 2000 && totalFeet <= 2500){farePrice = 25}
+
+    else farePrice = "cannot travel that far";
+    return farePrice
+}
+calculatesFarePrice(50,58);
